b80f6338054492268e7719d8a87b291b36df041b
anapao07 <annapao.arcos@gmail.com>
2019-12-23 17:46:02 -0600

    Survey ID

--- app/containers/Survey/index.js
+++ app/containers/Survey/index.js
@@ -9,9 +9,38 @@ import {
 
 import { WebView } from 'react-native';
 import * as userActions from "../../actions/user";
-let htmlview = require('./index.html');
+// let htmlview = require('./index.html');
 import HTMLView from 'react-native-htmlview';
 import WebViewBridge from 'react-native-webview-bridge';
+
+import { store, persistor } from '../../store/store';
+import { ApolloClient } from 'apollo-client'
+import {ApolloProvider} from 'react-apollo';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import storage from "../../utils/storage";
+
+const httpLink = createHttpLink({
+  uri: 'http://api.kliento.mx/v1/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = storage.get('access_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      'x-hasura-admin-secret': 'admin',
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
 
 
 
@@ -19,7 +48,9 @@ class Survey extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      item: {}
+      item: {
+       
+      }
 
     }
 
@@ -27,23 +58,45 @@ class Survey extends React.Component {
 
 
   render() {
+// console.log("AQUIESTA EL ID)",this.props.navigation.state.params.id);
+let ids = this.props.navigation.state.params.id;
 
-
-    var HtmlCode = '';
-
+    // const  HtmlCode = '<script type = "text/javascript" src = "./temp.js" ></script>';
+    const  HtmlCode = `<html>
+    <head>
+      <script type = "text/javascript" src = "./temp.js" ></script>
+    </head>
+    <body>
+      <div id="root"><h1>HOLA M</h1>
+  
+          
+      </div>
+   
+    </body>
+  </html>`;
+    const  runFirst =`document.body.style.backgroundcolor = 'red'`;
     console.log("entro1");
-let VIEW = require('./index.html');
-    return (
+let VIEW = require('../../assets/index.html');
+
+let link= 'http://encuestas.kliento.mx.s3-website-us-east-1.amazonaws.com/#/myview/'+ids;
+
+console.log("URL-->",link);
+
+return (
 
       <WebView
          
-      // source={VIEW}
-       
+    
+
+      source={{uri:link }}
+         // source={VIEW}
       // value={htmlContent}
         // source={{html: require('./content.js')()}}
-        source={require('./index.html')}
-        javaScriptEnabled={true} 
-        //  source={{ html: HtmlCode }}
+       // source={require('./index.html')}
+       
+        // injecrtedJavaScript={HTMLView}
+        // source={{ html: HtmlCode,baseUrl:'./' }}
+        // javaScriptEnabled={true} 
         // source={htmlview}
         // originWhitelist={[' * ']}
         // source={'./index.html'}
